5fb6ddf475c4fffb2429a8662f1cb96b51f6f0e8
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2013-11-08 12:09:46 -0800

    Testing Php code

--- StudentSystem.php
+++ StudentSystem.php
@@ -1,18 +1,41 @@
 <?php
 
-
 $con = mysqli_connect("127.0.0.1", "steven", "password");
-if($con->connect_errno){
-	echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+if ($con->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
 }
 
-$sql = "CREATE DATABASE IF NOT EXIST 'StudentSystem' DEFAULT SET utf8 COLLATE utf8_unicode_ci";
+$createStudentSystem = "CREATE DATABASE IF NOT EXISTS `StudentSystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
 
-if(mysqli_query($con, $sql)){
-	echo "Database StudentSystem created successfully\n";
+if (mysqli_query($con,$createStudentSystem)){
+  echo "Database StudentSystem created successfully\n";
+  }
+else{
+  echo "Error creating database: " . mysqli_error($con);
+ }
+
+$useStudentSystem = "USE 'StudentSystem'";
+if(mysqli_query($con, $useStudentSystem)){
+	echo "Using StuentSystem successfully\n";
 }
 else{
-	echo "Error creating database: " . mysqli_error($con);
+	echo "Error using database: " . mysqli_error($con);
+}
+
+$createStudentTable = "CREATE TABLE IF NOT EXIST 'STUDENT' (
+	'ID' INT(3) NOT NULL, 
+	'FirstName' VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
+	'LastName' VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
+	'Gender' VARCHAR(6) COLLATE utf8_unicode_ci NOT NULL,
+	'Grade' VARCHAR(15) COLLATE utf8_unicode_ci NOT NULL,
+	PRIMARY KEY ('ID')
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci";
+
+if(mysqli_query($con, $createStudentTable)){
+	echo "Create Student Table successfully\n";
+}
+else{
+	echo "Error Create table: " . mysqli_error($con);
 }
 
 
