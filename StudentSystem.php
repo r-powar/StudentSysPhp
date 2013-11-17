653069f4a169a8b92e43a309c3144554ddb41483
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2013-11-17 13:36:25 -0800

    Creating rest of the tables

--- StudentSystem.php
+++ StudentSystem.php
@@ -14,22 +14,30 @@ else{
   echo "Error creating database: " . mysqli_error($con);
  }
 
-$useStudentSystem = "USE 'StudentSystem'";
+$useStudentSystem = "USE `StudentSystem`";
 if(mysqli_query($con, $useStudentSystem)){
-	echo "Using StuentSystem successfully\n";
+        echo "Using StuentSystem successfully\n";
 }
 else{
-	echo "Error using database: " . mysqli_error($con);
+        echo "Error using database: " . mysqli_error($con);
 }
 
-$createStudentTable = "CREATE TABLE IF NOT EXIST 'STUDENT' (
-	'ID' INT(3) NOT NULL, 
-	'FirstName' VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
-	'LastName' VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
-	'Gender' VARCHAR(6) COLLATE utf8_unicode_ci NOT NULL,
-	'Grade' VARCHAR(15) COLLATE utf8_unicode_ci NOT NULL,
-	PRIMARY KEY ('ID')
-) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci";
+$dropStudentTable = "DROP TABLE IF EXISTS `STUDENT`";
+if(mysqli_query($con, $dropStudentTable)){
+	echo "Drop Student Table successfully\n";
+}
+else{
+	echo "Error dropping table: " . mysqli_error($con);
+}
+
+$createStudentTable = "CREATE TABLE IF NOT EXISTS `STUDENT` (
+	`ID` INT(3) NOT NULL, 
+	`FirstName` VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
+	`LastName` VARCHAR(20) COLLATE utf8_unicode_ci NOT NULL,
+	`Gender` VARCHAR(6) COLLATE utf8_unicode_ci NOT NULL,
+	`Grade` VARCHAR(15) COLLATE utf8_unicode_ci NOT NULL,
+	PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
 
 if(mysqli_query($con, $createStudentTable)){
 	echo "Create Student Table successfully\n";
@@ -38,6 +46,19 @@ else{
 	echo "Error Create table: " . mysqli_error($con);
 }
 
+$studentData = "INSERT INTO `STUDENT` (`ID`, `FirstName`, `LastName`, `Gender`, `Grade`) VALUES
+(123, 'Steven', 'Liao', 'Male', 'Senior'), 
+(789, 'Tieu', 'Nguyen', 'Male', 'Senior'), 
+(456, 'Raj', 'Powar', 'Male', 'Freshman'), 
+(234, 'Ron', 'Mak', 'Male', 'Senior'),
+(567, 'Suneuy', 'Kim', 'Female', 'Sophmore'),
+(666, 'Cay', 'Horstmann', 'Male', 'Freshman')";
 
+if(mysqli_query($con, $studentData)){
+	echo "INSERT INTO STUDENT Table successfully\n";
+}
+else{
+	echo "Error INSERT table: " . mysqli_error($con);
+}
 
 ?>
