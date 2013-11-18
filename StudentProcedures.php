b85a1aebeb567ac2d9985f212fed01d36284c879
smliao <stevenliao@Stevens-MacBook-Pro-4.local>
2013-11-17 18:03:28 -0800

    Added Some procedures to run

---
+++ StudentProcedures.php
@@ -0,0 +1,43 @@
+<?php
+
+function dbConnect(){
+	$con = mysqli_connect("localhost", "root", "", "StudentSystem");
+	if(mysqli_connect_errno($con)){
+		echo "Failed to connect";
+	}
+	return $con;
+}
+
+function getStudent($connection){
+	$statement = $connection->query("SELECT * FROM STUDENT");
+	return $statement;
+}
+
+function getDetailInfo($connection){
+	$statement = $connection->query("SELECT * FROM STUDENT NATURAL JOIN 
+		EXAMRESULT NATURAL JOIN SEMESTER NATURAL JOIN EXAMINFO NATURAL JOIN
+		ATTENDANCE NATURAL JOIN LABRESULT");
+	return $statement;
+}
+
+function getExamAverageScore($connection){
+	$statement = $connection->query("SELECT AVG(Score) FROM 
+		EXAMRESULT NATURAL JOIN STUDENT");
+	return $statement;
+}
+
+function getNumberOfRegisterStudent($connection){
+	$statement = $connection->query("SELECT count(DISTINCT ID) FROM STUDENT");
+	return $statement;
+}
+
+function getLabScores($connection){
+	$statement = $connection->query("SELECT ID, LabLow, LabHigh FROM LABRESULT");
+	return $statement;
+}
+
+function dbDisconnect($connection){
+	mysqli_close($connection);
+}
+
+?>
