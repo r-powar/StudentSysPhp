3d0d24fdd0c18e4090c4b7ade2e2e689f08f9a48
x0tn1x <nguyentieu@ymail.com>
2013-11-23 16:47:52 -0800

    modified view to display multiple queries

--- StudentIndex.php
+++ StudentIndex.php
@@ -8,6 +8,12 @@
     $(function() {
     $( "#tabs" ).tabs();
     });
+    
+    $(document).ready(function(){
+    $("#flip").click(function(){
+    $("#panel").slideToggle("slow");
+    });
+    });
   </script>
   
 
@@ -20,6 +26,25 @@
     border: 1px solid black;
     }
 
+    #flip {
+    width: 300px;
+    border: 2px solid black;
+    background-color:#01A9DB;
+    color:white;
+    text-align: center;
+    margin-left: auto ;
+    margin-right: auto ;
+    }
+
+    #panel {
+    text-align: center;
+    margin-left: auto ;
+    margin-right: auto ;
+    border: 1px solid black;
+    width:300px;
+    background-color: #E6E6E6;
+    display:none;
+    }
     tr:nth-child(even) {
     background-color: #81BEF7;
     }
@@ -40,11 +65,30 @@ if (!empty($_POST)){
   <div id="tabs">
     <ul>
       <li>
-        <a href="#tabs-1">Viewing Results</a>
+        <a href="#tabs-1">Exam Taking</a>
+      </li>
+      <li>
+        <a href="#tabs-2">Viewing Results</a>
       </li>
     </ul>
     <div id="tabs-1">
-      <a href="Procedures/StudentInfo.php" target="stu-iframe" style="border:1px solid black; background-color:green;color:yellow;">Detailed Student Information</a>
+      <form>
+        Q1) 1+1 = 2: <br></br>
+        <input type="radio" name="q1" value="True">True</input>
+        <input type="radio" name="q1" value="False">False</input><br></br>
+      </form>
+    </div>
+    <div id="tabs-2">
+      <div id="flip">Click To Select Query To Display</div>
+      <div id="panel">
+        <a href="Procedures/StudentInfo.php?link=1" target="stu-iframe" style="border:1px solid black; background-color:green;color:yellow;">Detailed Student Information</a>
+        <br></br>
+          <a href="Procedures/StudentInfo.php?link=2" target="stu-iframe" style="border:1px solid black; background-color:green;color:yellow;">Average Exam Score</a>
+        <br></br>
+        <a href="Procedures/StudentInfo.php?link=3" target="stu-iframe" style="border:1px solid black; background-color:green;color:yellow;">Etc.</a>
+        <br></br>
+      </div>
+      
       <iframe width="100%" height="100%" seamless="seamless" frameborder="0" name="stu-iframe" src="Procedures/StudentInfo.php"></iframe>
     </div>
   </div>
