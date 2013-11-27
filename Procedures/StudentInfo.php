43cc7cc8ef863d80501e1e17ff39515572abd7b8
x0tn1x <nguyentieu@ymail.com>
2013-11-26 21:26:46 -0800

    Update StudentInfo.php

--- Procedures/StudentInfo.php
+++ Procedures/StudentInfo.php
@@ -124,11 +124,11 @@ mysqli_close($con);
 
 if( $link =='5' ){
     echo "You Selected Link 5!";
-  if($_POST){
+  if($_POST) {
     $Term = $_POST['Term'];
     $Term = htmlspecialchars($Term);
     $result = mysqli_query($con,"CALL `countStudentByTerm` ('$Term');");
-    
+   }
 echo "<table border='1'>
 <tr>
 <th>Number Student in Current Term</th>
@@ -145,9 +145,139 @@ echo "</table>";
 
 mysqli_close($con);
 }
+
+if ($link == '6') {
+	echo 'You selected Link 6';
 }
 
+if ($link == '7') {
+	echo 'You selected Link 7';
+}
 
+if( $link =='8' ){
+    echo "You Selected Link 8!";
+	$score = $_POST['labfail'];
+    $result = mysqli_query($con, "SELECT firstname, lastname, labpassfail, score from student, labresult, examresult where labpassfail = '1' AND score '". $score ."'");
+    
+echo "<table border='1'>
+<tr>
+<th>FirstName</th>
+<th>LastName</th>
+<th>LabPassFail</th>
+<th>Exam Score</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+  echo "<tr>";
+  echo "<td>" . $row['firstname'] . "</td>";
+  echo "<td>" . $row['lastname'] . "</td>";
+  echo "<td>" . $row['labpassfail'] . "</td>";
+  echo "<td>" . $row['score'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+}
+
+if ($link == '9') {
+	echo "You selected Link 9";
+    $result = mysqli_query($con, "SELECT firstname, lastname, lettergrade, absences from student, examresult, attendance where lettergrade = 'A' AND absences > 0");
+    
+echo "<table border='1'>
+<tr>
+<th>FirstName</th>
+<th>LastName</th>
+<th>LetterGrade</th>
+<th>Absences</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+  echo "<tr>";
+  echo "<td>" . $row['firstname'] . "</td>";
+  echo "<td>" . $row['lastname'] . "</td>";
+  echo "<td>" . $row['lettergrade'] . "</td>";
+  echo "<td>" . $row['absences'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+}
+
+if ($link == '10') {
+	echo "You selected Link 10";
+    $result = mysqli_query($con, "SELECT grade, score from student, examresult where grade = 'Freshman' OR grade = 'Senior' AND score > 0");
+    
+echo "<table border='1'>
+<tr>
+<th>Grade</th>
+<th>Score</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+  echo "<tr>";
+  echo "<td>" . $row['grade'] . "</td>";
+  echo "<td>" . $row['score'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+}
+
+if ($link == '11') {
+	echo "You selected Link 11";
+    $result = mysqli_query($con, "SELECT gender, score, absences, labpassfail from student, examresult, attendance, labresult where gender = 'Male' OR gender = 'Female'");
+    
+echo "<table border='1'>
+<tr>
+<th>Gender</th>
+<th>Score</th>
+<th>Absences</th>
+<th>LabPassFail</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+  echo "<tr>";
+  echo "<td>" . $row['gender'] . "</td>";
+  echo "<td>" . $row['score'] . "</td>";
+  echo "<td>" . $row['absences'] . "</td>";
+  echo "<td>" . $row['labpassfail'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+}
+
+if ($link == '12') {
+	echo "You selected Link 12";
+    $result = mysqli_query($con, "SELECT labpassfail, term, lastname from labresult, semester, student where labpassfail = '1' or labpassfail ='0' and term = 'Spring' or term = 'Fall'");
+    
+echo "<table border='1'>
+<tr>
+<th>LabPassFail</th>
+<th>Term</th>
+<th>LastName</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+  echo "<tr>";
+  echo "<td>" . $row['labpassfail'] . "</td>";
+  echo "<td>" . $row['term'] . "</td>";
+  echo "<td>" . $row['lastname'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+}
 
 
 ?>
